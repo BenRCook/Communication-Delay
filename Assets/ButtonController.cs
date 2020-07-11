a4db0ba5244cb5ff7cad00cba5bb8eb653a0a6ff
Ben <40598265+BenRCook@users.noreply.github.com>
2020-07-11 19:56:07 +0100

    made button controller

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public String currentButton;
+
+    private void OnMouseDown()
+    {
+        switch (currentButton)
+        {
+            case "move":
+                Console.WriteLine("move(" + Input.mousePosition + ")");
+                break;
+            case "missile":
+                Console.WriteLine("Missile(" + Input.mousePosition + ")");
+                break;
+            case "kinetic":
+                Console.WriteLine("Kinetic(" + Input.mousePosition + ")");
+                break;
+            case "lazer":
+                Console.WriteLine("Missile(" + Input.mousePosition + ")");
+                break;
+            default:
+                Console.WriteLine("This Button Does Nothing");
+                break;
+        }
+    }
+
+    public void ButtonPress(String buttonType)
+    {
+        currentButton = buttonType;
+    }
+}
